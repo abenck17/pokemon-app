bae28ae887c3b2ee3056cde1e1c1f605744940bd
Austin Benckendorf <BenckendorfAustin@johndeere.com>
2020-10-16 16:34:35 -0500

    complete CRUD functionality

--- server.js
+++ server.js
@@ -1,21 +1,61 @@
 const express = require('express');
-const app = express();//app is an object
-
-const pokemon = require('./models/pokemon.js'); //NOTE: it must start with ./ if it's just a file, not an NPM package
+const app = express();
 
 
-app.get('/pokemon/:index', (req, res) => { // :index can be anything
-    res.render('show.ejs', { //second param must be an object
-        pokemon: pokemon[req.params.index] //there will be a variable available inside the ejs file called fruit, its value is fruits[req.params.index]
+const methodOverride = require('method-override');
+
+
+app.use(methodOverride('_method'));
+
+app.use((req, res, next) => {
+    console.log('I run for all routes');
+    next();
+});
+
+
+app.use(express.urlencoded({ extended: true }));
+
+const pokemon = require('./models/pokemon.js'); 
+
+
+app.get('/pokemon/new', (req, res) => {
+    res.render('new.ejs');
+});
+
+app.get('/pokemon/:index', (req, res) => { 
+    res.render('show.ejs', { 
+        pokemon: pokemon[req.params.index] 
     });
 });  
 
-// GET ==> Route to our homepage, and user can click on url to create a new fruit 
+ 
 app.get('/pokemon', (req, res) => {
     res.render('index.ejs', {
-        pokemon: pokemon //there will be a variable available inside the ejs file called pokemons, its value is pokemon[req.params.index]
-    }); // {} callback function
+        pokemon: pokemon 
+    }); 
     
+});
+
+app.post('/pokemon', (req, res) => {
+    pokemon.push(req.body);
+    res.redirect('/pokemon');
+});
+
+app.delete('/pokemon/:index', (req, res) => {
+	pokemon.splice(req.params.index, 1); 
+	res.redirect('/pokemon');  
+});
+
+app.get('/pokemon/:index/edit', function(req, res){
+	res.render('edit.ejs', { 
+		pokemon: pokemon[req.params.index], 
+		index: req.params.index 
+    });
+});
+
+app.put('/pokemon/:index', (req, res) => { 
+    pokemon[req.params.index] = req.body;
+    res.redirect('/pokemon'); 
 });
 
 app.listen(3000, ()=>{
